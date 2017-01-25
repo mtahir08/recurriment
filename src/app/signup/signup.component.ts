92a9082634683726e2e0501b6169546c72b4406f
mtahir08 <mtahir.ps@gmail.com>
2017-01-26 00:58:25 +0500

    firebase login added

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -24,19 +24,41 @@ export class SignupComponent implements OnInit {
     ]
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   signup() {
-    this.fb.auth.createUser({ email: this.email, password: this.password })
-    .then(data => {
-
-    })
-    .catch(error=>{
-      
-    });
+    let promise = this.fb.auth.createUser({ email: this.email, password: this.password });
+    promise
+      .then(data => { console.log('signup success', data); this.login(data); })
+      .catch(err => console.log('signup error', err));
   }
 
+  login(signupData) {
 
+    let promise = this.fb.auth.login({ email: this.email, password: this.password },
+      { provider: AuthProviders.Password, method: AuthMethods.Password });
 
+    promise
+      .then(data => { console.log('login success', data); this.createUserNode(signupData, data); })
+      .catch(err => console.log('login error', err));
+  }
 
+  createUserNode(signupData, loginData) {
+    let obj = {
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      type: this.selectedValue,
+      login: true,
+      createdAt: this.fb.database.ServerValue.TIMESTAMP,
+      updatedAt:this.fb.database.ServerValue.TIMESTAMP
+    }
+
+    let promise = this.fb.database.object('/users');
+    promise.set(obj);
+    promise
+      .then(data => console.log('data saved', data))
+      .catch(err => console.log('data saving error', err));
+  }
+  
 }
