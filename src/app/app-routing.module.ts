4907afd763d1ba4b10ee653afc22d584e83f1a6b
mtahir08 <mtahir.ps@gmail.com>
2017-01-26 02:25:42 +0500

    path added for dashboard, admin and company

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { CompanyComponent } from './company/company.component';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 // import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes:Routes = [
@@ -15,6 +18,15 @@ const routes:Routes = [
   // {
   //   path: 'forgotPassword', component: ForgotPasswordComponent, children: []
   // },
+  {
+    path: 'dashboard', component: DashboardComponent, children: []
+  },
+  {
+    path: 'company', component: CompanyComponent, children: []
+  },
+  {
+    path: 'admin', component: AdminComponent, children: []
+  },
   {
     path: '', redirectTo: 'login', pathMatch: 'full'
   }
