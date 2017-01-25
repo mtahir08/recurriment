a73ad3bb599883690c9c22d25ec1df532344e168
mtahir08 <mtahir.ps@gmail.com>
2017-01-25 21:00:29 +0500

    login view updated and properties added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  email: string;
+  password: string;
+  constructor() {
+    this.email = "abc@gmail.com";
+    this.password = "123456";
+  }
 
   ngOnInit() {
   }
 
+  login() {
+
+  }
 }
