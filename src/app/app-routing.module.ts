6ec2696b6ca173ed62b3d81872b836f7a04f70d9
mtahir08 <mtahir.ps@gmail.com>
2017-01-26 18:59:17 +0500

    route updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginGuardService } from './login-guard.service'
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { CompanyComponent } from './company/company.component';
 import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-// import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
-const routes:Routes = [
+const routes: Routes = [
   {
     path: 'login', component: LoginComponent, children: []
   },
   {
     path: 'signup', component: SignupComponent, children: []
   },
-  // {
-  //   path: 'forgotPassword', component: ForgotPasswordComponent, children: []
-  // },
   {
-    path: 'dashboard', component: DashboardComponent, children: []
+    path: 'forgotPassword', component: ForgotPasswordComponent, children: []
   },
   {
-    path: 'company', component: CompanyComponent, children: []
+    path: 'dashboard', component: DashboardComponent,canActivate: [LoginGuardService]
   },
   {
-    path: 'admin', component: AdminComponent, children: []
+    path: 'company', component: CompanyComponent, children: [], canActivate: [LoginGuardService]
+  },
+  {
+    path: 'admin', component: AdminComponent, canActivate: [LoginGuardService]
   },
   {
     path: '', redirectTo: 'login', pathMatch: 'full'
