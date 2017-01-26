61630f76f7112725905243c2c058cf980a7419f7
mtahir08 <mtahir.ps@gmail.com>
2017-01-26 19:54:30 +0500

    posted jobs data view and function added

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
 
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
@@ -8,16 +10,19 @@ import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 
 })
 export class CompanyComponent implements OnInit {
 
-students;
-// sidenav.toggle()
-  constructor(public fb: AngularFire) {
+  students;
+  jobs;
+  // sidenav.toggle()
+  constructor(public fb: AngularFire, private router: Router, private authService: AuthService) {
     this.students = [];
-   }
+    this.jobs = [];
+  }
 
   ngOnInit() {
   }
 
   showStudents(sidenav: any) {
+    this.jobs.length = 0;
     let jobs = this.fb.database.list('/users', {
       query: {
         orderByChild: 'type',
@@ -29,5 +34,27 @@ students;
       sidenav.toggle();
     });
 
+  }
+
+  showPostedJobs(sidenav: any) {
+    this.students.length = 0;
+    let currentUser = this.authService.getUserData();
+    console.log(currentUser['uid']);
+    let jobs = this.fb.database.list('/jobs', {
+      query: {
+        orderByChild: 'companyId',
+        equalTo: currentUser['uid']
+      }
+    }).subscribe(data => {
+      console.log(data);
+      this.jobs = data;
+      sidenav.toggle();
+    });
+  }
+
+   signout() {
+    this.authService.doUnsubscribe();
+
+    this.router.navigate(['/login'])
   }
 }
